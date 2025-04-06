ff5f97648af7ff8660999ce11e9ea1a52dbe543d
hitmorecode <38550613+hitmorecode@users.noreply.github.com>
2025-04-06 04:14:26 +0200

    js first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function showSocialMedia (e) {
+  let btnSocialMedia = document.querySelector(".social-media-share");
+  let socialMediaLinks = document.querySelector(".no-display");
+
+  btnSocialMedia.addEventListener("click", function () {
+    if (socialMediaLinks.getAttribute("class") == "no-display") {
+      socialMediaLinks.setAttribute("class", "social-media");
+    }
+    else {
+      socialMediaLinks.setAttribute("class", "no-display");
+    }
+  });
+  return e;
+}
+
+let showSocialMediaLinks = showSocialMedia ();
